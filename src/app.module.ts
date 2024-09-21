a5b39b2b0cae3431ff57a60e05b3418e736879e7
Mateus leonardo <mateus_leonardo1997@hotmail.com>
2024-09-21 17:14:58 -0300

    style: adicionando configmodule como primeiro import

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,16 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { GuestModule } from './guest/guest.module';
 
 @Module({
   imports: [
-    UserModule,
-    AuthModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    UserModule,
+    AuthModule,
+    GuestModule,
   ],
   controllers: [AppController],
   providers: [AppService],
