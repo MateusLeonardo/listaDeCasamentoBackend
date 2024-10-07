6bb8e50079ae7ec4abc3a67655f0df730422523c
Mateus leonardo <mateus_leonardo1997@hotmail.com>
2024-10-06 22:47:28 -0300

    feat: adicionando rota para validação de token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,15 @@
-import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Req,
+  Res,
+  UnauthorizedException,
+  UseGuards,
+} from '@nestjs/common';
 import { Response } from 'express';
 import { AuthService } from './auth.service';
 import { GoogleOauthGuard } from './guards/google-oauth.guard';
+import { Token } from 'src/decorators/token-decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -23,7 +31,24 @@ export class AuthController {
       secure: false,
     });
 
-    return res.redirect('/');
+    return res.redirect('http://localhost:3001/home');
+  }
+
+  @Get('validate-token')
+  async validateToken(
+    @Token('access_token') token: string,
+    @Res() res: Response,
+  ) {
+    if (!token) {
+      throw new UnauthorizedException('Token não fornecido');
+    }
+
+    try {
+      const decoded = await this.authService.validateJwt(token);
+      return res.status(200).json({ valid: true, decoded });
+    } catch (error) {
+      return res.status(401).json({ valid: false, message: error.message });
+    }
   }
 
   @Get('logout')
